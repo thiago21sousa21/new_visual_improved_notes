673cad1649245effa1a93333810f811854abc993
thiago21sousa21 <thiago21sousa21@gmail.com>
2025-06-04 17:25:49 -0300

    first commit

---
+++ ToLearn/meus_comandos.lua
@@ -0,0 +1,32 @@
+-- Mostra os números das linhas na lateral esquerda
+vim.opt.number = true
+-- Mostra a diferença de linhas relativa a linha atual
+vim.opt.relativenumber = true
+-- usa espaços em vez de tabs
+vim.opt.expandtab = true
+-- número de espaços ao usar identação automática
+vim.opt.shiftwidth = 2
+-- número de espaços ao apertar tab
+vim.opt.tabstop = 2
+-- Ativa identação inteligente (com base no tipo de código e arquivo)
+vim.opt.smartindent = true
+-- ativa identação automática ( repete a identação da linha anterior)
+vim.opt.autoindent = true
+-- destaca a linha onde o cursor está
+vim.opt.cursorline = true
+-- ignora maiuscular/minusculas nas buscas
+vim.opt.ignorecase = true
+-- se tiver uma letra maiuscula na busca, tornar a busca sensivel
+vim.opt.smartcase = true
+-- mostra a busca enquanto você digita
+vim.opt.incsearch = true
+-- não mantém os resultados de buscas destacados após ela terminar
+vim.opt.hlsearch = false
+-- ativa o uso do mouse: click, seleção etc
+vim.opt.mouse = "a"
+-- ativa a corers 24 bits no terminal
+vim.opt.termguicolors = true
+-- abre novas janelas abaixo da atual (em vez de cima)
+vim.opt.splitbelow = true
+-- abre novas janelas a direita da atual (em vez da esquerda)
+vim.opt.splitright = true
